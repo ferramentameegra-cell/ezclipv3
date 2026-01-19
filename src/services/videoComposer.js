6490cf9f4b3e82fe726d4c1305543391347e6228
Josyas Borba <josyasborba@MacBook-Air-de-Josyas.local>
2026-01-19 14:29:41 -0300

    fix: adicionar import do streamableUtils no videoComposer

--- src/services/videoComposer.js
+++ src/services/videoComposer.js
@@ -17,6 +17,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { getRetentionVideoPath, getRandomRetentionVideoPath } from './retentionVideoManager.js';
 import { RETENTION_VIDEOS, NICHES } from '../models/niches.js';
+import { convertStreamableToDirectUrl, isStreamableUrl } from '../utils/streamableUtils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
